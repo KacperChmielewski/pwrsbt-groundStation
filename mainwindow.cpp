2a36cbd8427b1e0b3f7706ad40f2ad4b5d5a282d
Kacper Chmielewski <kacper.chmielewski@blebox.eu>
2019-06-09 19:01:16 +0200

    mqtt

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -45,6 +45,58 @@ MainWindow::MainWindow(QWidget *parent) :
     mClient->setHostname(ui->hostLine->text());
     mClient->setPort((qint16) ui->hostLine->text().toInt()); // TODO: implicit cast
 
+    mClient->setPort(1883);
+    mClient->setHostname(QString("assistantintegration.dev.blebox.eu"));
+
+    const QString content = QLatin1String(": State Change")
+                    + QString::number(mClient->state())
+                    + QLatin1Char('\n');
+    log->print(content);
+
+    connect(mClient, &QMqttClient::stateChanged,  this, [this]() {
+//        extern logger* log;
+//        log->print("status");
+        std::cout << "status" << std::endl;
+
+    });
+
+
+    connect(mClient, &QMqttClient::messageReceived, this, [this](const QByteArray &message, const QMqttTopicName &topic) {
+//        extern logger* log;
+        const QString content = QLatin1String(" Received Topic: ")
+                    + topic.name()
+                    + QLatin1String(" Message: ")
+                    + message
+                    + QLatin1Char('\n');
+        std::cout << "content" << std::endl;
+//        log->print(content);
+    });
+
+
+
+    if (mClient->state() == QMqttClient::Disconnected) {
+//        ui->lineEditHost->setEnabled(false);
+//        ui->spinBoxPort->setEnabled(false);
+//        ui->buttonConnect->setText(tr("Disconnect"));
+        mClient->connectToHost();
+        log->print("Laczenie");
+
+        auto subscription = mClient->subscribe(QMqttTopicFilter("sb/speed/pitot"));
+        if (!subscription) {
+            log->print("Could not subscribe. Is there a valid connection?");
+            return;
+        }
+
+
+    } else {
+//        ui->lineEditHost->setEnabled(true);
+//        ui->spinBoxPort->setEnabled(true);
+//        ui->buttonConnect->setText(tr("Connect"));
+        mClient->disconnectFromHost();
+        log->print("razlaczenie");
+
+    }
+
 
 ////    auto subscription = m_client->subscribe(QMqttTopicFilter(QLatin1String("sb/speed/pitot")));
 ////    if (!subscription) {
