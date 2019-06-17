56b427805976811e4b6c83383cfe9572ffd8f0a2
Kacper Chmielewski <kacper.chmielewski@blebox.eu>
2019-06-17 03:31:09 +0200

    logger doc

--- logger.h
+++ logger.h
@@ -1,3 +1,9 @@
+/*!
+* \file
+* \brief Definicja klasy dziennika zdarzeń
+*
+* Plik zawiera definicję klasy Logger
+*/
 #ifndef LOGGER_H
 #define LOGGER_H
 
@@ -5,7 +11,12 @@
 
 #include <QPlainTextEdit>
 #include <QPushButton>
-
+/*!
+* \brief Obsługa dziennika zdarzeń
+*
+* Klasa jest obsługuje elementy dziennika zdarzeń i udostępnia
+* metody jego obsługi.
+*/
 class Logger : public QObject
 {
     Q_OBJECT
@@ -14,13 +25,42 @@ private:
     QPushButton* button;
 public:
     explicit Logger(QObject *parent = nullptr);
+    /*!
+     * \brief Przypisanie elementów interfejsu
+     *
+     * Podpina elementy interfejsu, które będą używane jako dziennik zdarzeń
+     * \param[in] txt - okno loggera
+     * \param[in] btn - przycisk kasujący dane
+     */
     void setUI(QPlainTextEdit *txt, QPushButton *btn);
+    /*!
+     * \brief Pisanie do loggera
+     *
+     * Drukuje podany ciąg w oknie dziennika zdarzeń
+     * \param[in] text - dane do wydrukowania
+     */
     void print(QString text);
+    /*!
+     * \brief Pisanie do loggera
+     *
+     * Drukuje podany ciąg w oknie dziennika zdarzeń
+     * \param[in] text - dane do wydrukowania
+     */
     void print(const char* text);
+    /*!
+     * \brief Czyszczenie
+     *
+     * Czyszczenie okna dziennika zdarzeń
+     */
     void clean();
 signals:
 
 public slots:
+    /*!
+     * \brief Obsługa kliknięcia "clean"
+     *
+     * Obsługuje zdarzenie kliknięcia "clean"
+     */
     void cleanButtonClicked();
 };
 
