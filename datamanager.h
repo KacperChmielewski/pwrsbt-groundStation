8b7c6f0664a7e937e8bd727e260fe7782ce70411
Kacper Chmielewski <kacper.chmielewski@blebox.eu>
2019-06-17 03:40:53 +0200

    Data manager doc

--- datamanager.h
+++ datamanager.h
@@ -1,3 +1,10 @@
+/*!
+* \file
+* \brief Definicja klasy menedżera danych
+*
+* Plik zawiera definicję klasy DataManager
+*/
+
 #ifndef DATAMANAGER_H
 #define DATAMANAGER_H
 
@@ -10,7 +17,11 @@
 #include <QPushButton>
 
 #include "logger.h"
-
+/*!
+* \brief Zarządca danych
+*
+* Klasa jest odpowiedzialna za odbiór danych.
+*/
 class DataManager : public QObject
 {
     Q_OBJECT
@@ -29,14 +40,50 @@ private:
 
 public:
     explicit DataManager(QObject *parent = nullptr);
+    /*!
+     * \brief Przypisanie elementów interfejsu
+     *
+     * Podpina elementy interfejsu, które będą używane jako konfiguracja zarządcy danych
+     * \param[in] host - okno hosta
+     * \param[in] port - okno portu
+     * \param[in] connect - przycisk do połączenia
+     */
     void setUI(QLineEdit* host, QLineEdit* port, QPushButton* connect);
+    /*!
+     * \brief Podpięcie loggera
+     *
+     * Podpina dziennik zdarzeń do zdarzeń z zarządcy danych
+     * \param[in] l - wskaźnik do loggera
+     */
     void setLogger(Logger* l);
+    /*!
+     * \brief Informacje o połączeniu
+     *
+     * Drukuje na dzienniku zdarzeń aktualne informacje o połączeniu z serwerem danych
+     */
     void mqttStatusReport();
 
 signals:
+    /*!
+     * \brief Sygnał otrzymania danych
+     *
+     * Sygnal wysyłany po otrzymaniu danych z serwera. Dane przesyłane są w postaci nagłowka i wartości.
+     * \param[in] head - nagłówek danych
+     * \param[in] value - dane
+     */
     void pushData(QString head, QString value);
 public slots:
+    /*!
+     * \brief Obsługa kliknięcia "Connect"
+     *
+     * Obsługuje zdarzenie kliknięcia "Connect"
+     */
     void connectButtonClicked();
+    /*!
+     * \brief Obsługa zmiany stanu połączenia
+     *
+     * Obsługuje zdarzenie zmiany stanu połączenia z serwerem danych
+     */
     void mqttStatusChanged();
 
 };
